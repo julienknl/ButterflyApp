58b932fdf4bae6212051ba8cbb1e2bf69673e358
Julien KNell <julienknell@outlook.com>
2022-11-27 16:58:56 +0400

    Implemented the table view for the list of product

--- Butterfly/Controllers/Home/HomeViewController.swift
+++ Butterfly/Controllers/Home/HomeViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var products: [ProductOrder] = [] {
+        didSet {
+            //Use the main UI thread to load the data
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    private let cellIdentifier = "ProductCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        //Init the nib to use the custom cell
+        let nib = UINib(nibName: "ProductTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: cellIdentifier)
     }
 
+}
 
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ProductTableViewCell else { return UITableViewCell() }
+        
+        cell.set(data: products[indexPath.row])
+        
+        return cell
+    }
+    
 }
 
---
+++ Butterfly/Views/CustomCell/ProductTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProductTableViewCell.swift
+//  Butterfly
+//
+//  Created by Julien on 27/11/2022.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func set(data: ProductOrder) {
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
